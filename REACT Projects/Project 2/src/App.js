259837ec5cebb7f0838163d2fdbc367e300b074c
shreyash9806 <skumar60_be18@thapar.edu>
2023-08-03 10:34:46 +0530

    Done project 2 uploading \

---
+++ REACT Projects/Project 2/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import './3.forms and hooks/style.css'
+// import Userform from './3.forms and hooks/Userform'
+// import UseEffectHook from './3.forms and hooks/UseEffectHook'
+// import Hooks from './3.forms and hooks/Hooks'
+import NewsApp from './3.forms and hooks/NewsApp'
+function App() {
+
+  return (
+    <div>
+      {/* <Userform /> */}
+      {/* <UseEffectHook /> */}
+      {/* <Hooks /> */}
+      <NewsApp />
+    </div>
+  )
+}
+
+export default App
